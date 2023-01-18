f2675ba0e49555f09b7263d7564eb95e0e334d69
Laptop <stark2542@gmail.com>
2023-01-18 20:25:35 +0200

    Init commit

---
+++ src/main/java/com/karen/KarenDataApplication.java
@@ -0,0 +1,15 @@
+package com.karen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class KarenDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KarenDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/karen/KarenDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KarenDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

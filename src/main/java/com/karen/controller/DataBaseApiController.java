68081e03d84ea447c6e6a38483b6f814e3733b68
Laptop <stark2542@gmail.com>
2023-01-22 23:32:46 +0200

    Add: static varibles for endpoints

--- src/main/java/com/karen/controller/DataBaseApiController.java
+++ src/main/java/com/karen/controller/DataBaseApiController.java
@@ -1,5 +1,6 @@
 package com.karen.controller;
 
+import com.karen.endpoints.TempEndpoints;
 import com.karen.model.Temperature;
 import com.karen.service.DateService;
 import lombok.AllArgsConstructor;
@@ -14,17 +15,17 @@ public class DataBaseApiController {
 
     private final DateService dateService;
 
-    @PostMapping("/api/temp")
+    @PostMapping(TempEndpoints.API_TEMP)
     public Temperature addTemp(@RequestParam(value = "degrees", required = false) int degrees) {
         return dateService.saveTemperature(degrees);
     }
 
-    @GetMapping("/api/temps")
+    @GetMapping(TempEndpoints.API_TEMPS)
     public List<Temperature> getTemps() {
         return dateService.getTemperature();
     }
 
-    @DeleteMapping("/api/temps")
+    @DeleteMapping(TempEndpoints.API_TEMPS)
     public Map<String, Boolean> deleteTemps() {
         return dateService.deleteTemps();
     }
